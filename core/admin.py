332761e2f2bfb1066357aff375d3570044265461
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2024-04-17 21:44:45 +0300

    Add files via upload

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from .models import CustomUser,Profile,Movie,Video
+from .models import Movie, MovieList
 
 # Register your models here.
-admin.site.register(CustomUser)
-admin.site.register(Profile)
 admin.site.register(Movie)
-admin.site.register(Video)
+admin.site.register(MovieList)
--- core/views.py
+++ core/views.py
@@ -1,104 +1,141 @@
-from django.shortcuts import redirect, render
-from django.views import View
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from .models import Movie, MovieList
 from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
-from .forms import ProfileForm
-from .models import Movie, Profile
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+import re
 
-class Home(View):
-    def get(self,request,*args, **kwargs):
-        if request.user.is_authenticated:
-            return redirect(to='/profile/')
-        return render(request,'index.html')
+# Create your views here.
+@login_required(login_url='login')
+def index(request):
+    movies = Movie.objects.all()
+    featured_movie = movies[len(movies)-1]
 
-@method_decorator(login_required,name='dispatch')
-class ProfileList(View):
+    context = {
+        'movies': movies,
+        'featured_movie': featured_movie,
+    }
+    return render(request, 'index.html', context)
+
+@login_required(login_url='login')
+def movie(request, pk):
+    movie_uuid = pk
+    movie_details = Movie.objects.get(uu_id=movie_uuid)
+
+    context = {
+        'movie_details': movie_details
+    }
+
+    return render(request, 'movie.html', context)
+
+@login_required(login_url='login')
+def genre(request, pk):
+    movie_genre = pk
+    movies = Movie.objects.filter(genre=movie_genre)
+
+    context = {
+        'movies': movies,
+        'movie_genre': movie_genre,
+    }
+    return render(request, 'genre.html', context)
+
+@login_required(login_url='login')
+def search(request):
+    if request.method == 'POST':
+        search_term = request.POST['search_term']
+        movies = Movie.objects.filter(title__icontains=search_term)
+
+        context = {
+            'movies': movies,
+            'search_term': search_term,
+        }
+        return render(request, 'search.html', context)
+    else:
+        return redirect('/')
+
+@login_required(login_url='login')
+def my_list(request):
+
+    movie_list = MovieList.objects.filter(owner_user=request.user)
+    user_movie_list = []
+
+    for movie in movie_list:
+        user_movie_list.append(movie.movie)
+
+    context = {
+        'movies': user_movie_list
+    }
+    return render(request, 'my_list.html', context)
+
+@login_required(login_url='login')
+def add_to_list(request):
+    if request.method == 'POST':
+        movie_url_id = request.POST.get('movie_id')
+        uuid_pattern = r'[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}'
+        match = re.search(uuid_pattern, movie_url_id)
+        movie_id = match.group() if match else None
+
+        movie = get_object_or_404(Movie, uu_id=movie_id)
+        movie_list, created = MovieList.objects.get_or_create(owner_user=request.user, movie=movie)
+
+        if created:
+            response_data = {'status': 'success', 'message': 'Added ✓'}
+        else:
+            response_data = {'status': 'info', 'message': 'Movie already in list'}
+
+        return JsonResponse(response_data)
+    else:
+        # return error
+        return JsonResponse({'status': 'error', 'message': 'Invalid request'}, status=400)
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('login')
     
-    def get(self,request,*args, **kwargs):
+    return render(request, 'login.html')
 
-        profiles=request.user.profiles.all()
+def signup(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        password2 = request.POST['password2']
 
-        print(profiles)
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
 
+                # log user in
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+                return redirect('/')
+        else:
+            messages.info(request, 'Password Not Matching')
+            return redirect('signup')
+    else:
+        return render(request, 'signup.html')
 
-        return render(request,'profileList.html',{
-            'profiles':profiles
-        })
-
-
-@method_decorator(login_required,name='dispatch')
-class ProfileCreate(View):
-    def get(self,request,*args, **kwargs):
-        form=ProfileForm()
-
-        return render(request,'profileCreate.html',{
-            'form':form
-        })
-
-    def post(self,request,*args, **kwargs):
-        form=ProfileForm(request.POST or None)
-
-       
-        if form.is_valid():
-            print(form.cleaned_data)
-            profile = Profile.objects.create(**form.cleaned_data)
-            if profile:
-                request.user.profiles.add(profile)
-                return redirect(f'/watch/{profile.uuid}')
-
-        return render(request,'profileCreate.html',{
-            'form':form
-        })
-
-@method_decorator(login_required,name='dispatch')
-class Watch(View):
-    def get(self,request,profile_id,*args, **kwargs):
-        try:
-            profile=Profile.objects.get(uuid=profile_id)
-
-            movies=Movie.objects.filter(age_limit=profile.age_limit)
-
-            try:
-                showcase=movies[0]
-            except :
-                showcase=None
-            
-
-            if profile not in request.user.profiles.all():
-                return redirect(to='core:profile_list')
-            return render(request,'movieList.html',{
-            'movies':movies,
-            'show_case':showcase
-            })
-        except Profile.DoesNotExist:
-            return redirect(to='core:profile_list')
-
-
-@method_decorator(login_required,name='dispatch')
-class ShowMovieDetail(View):
-    def get(self,request,movie_id,*args, **kwargs):
-        try:
-            
-            movie=Movie.objects.get(uuid=movie_id)
-
-            return render(request,'movieDetail.html',{
-                'movie':movie
-            })
-        except Movie.DoesNotExist:
-            return redirect('core:profile_list')
-
-@method_decorator(login_required,name='dispatch')
-class ShowMovie(View):
-    def get(self,request,movie_id,*args, **kwargs):
-        try:
-            
-            movie=Movie.objects.get(uuid=movie_id)
-
-            movie=movie.videos.values()
-            
-
-            return render(request,'showMovie.html',{
-                'movie':list(movie)
-            })
-        except Movie.DoesNotExist:
-            return redirect('core:profile_list')
+@login_required(login_url='login')
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
